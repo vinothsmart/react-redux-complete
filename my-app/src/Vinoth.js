bfbc857f98589dbe8d3b657fa05ecb0e97e7957a
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-29 19:24:47 +0530

    added ternary operator cond based on age

--- my-app/src/Vinoth.js
+++ my-app/src/Vinoth.js
@@ -3,18 +3,27 @@ import React from "react";
 function Vinoth(props) {
   //   const { name, age, from } = props;
   const { namelists } = props.namelists;
+  //   const nameList = namelists.map((namelist) => {
+  //     if (namelist.age > 20) {
+  //       return (
+  //         <div className="vinoth" key={namelist.id}>
+  //           <div>Name : {namelist.name}</div>
+  //           <div>Age : {namelist.age} </div>
+  //           <div>From : {namelist.from}</div>
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
   const nameList = namelists.map((namelist) => {
-    if (namelist.age > 20) {
-      return (
-        <div className="vinoth" key={namelist.id}>
-          <div>Name : {namelist.name}</div>
-          <div>Age : {namelist.age} </div>
-          <div>From : {namelist.from}</div>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return namelist.age > 20 ? (
+      <div className="vinoth" key={namelist.id}>
+        <div>Name : {namelist.name}</div>
+        <div>Age : {namelist.age} </div>
+        <div>From : {namelist.from}</div>
+      </div>
+    ) : null;
   });
   return <div className="vinothList">{nameList}</div>;
 }
