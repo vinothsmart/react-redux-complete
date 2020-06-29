04c8dc6412770dde94ca549e463055a20326010f
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-29 18:41:04 +0530

    list all users in map func

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Vinoth from "./Vinoth";
 
 function App() {
+  const [names, setNames] = useState({
+    namelists: [
+      { name: "Vinoth", age: 30, from: "Kumbakonam", id: 1 },
+      { name: "Anand", age: 30, from: "Kumbakonam", id: 2 },
+      { name: "Kanna", age: 30, from: "Kumbakonam", id: 3 },
+    ],
+  });
   return (
     <div className="App">
       <h1>My React App!!</h1>
       <p>Welcome : )</p>
-      <Vinoth name="TempleCityVino" age="30" from="Kumbakonam" />
-      <Vinoth name="Kanna" age="30" from="Kumbakonam" />
+      {/* <Vinoth name="TempleCityVino" age="30" from="Kumbakonam" />
+      <Vinoth name="Kanna" age="30" from="Kumbakonam" /> */}
+      <Vinoth namelists={names} />
     </div>
   );
 }
--- my-app/src/Vinoth.js
+++ my-app/src/Vinoth.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 function Vinoth(props) {
-  const { name, age, from } = props;
-  return (
-    <div className="vinoth">
-      <div>Name : {name}</div>
-      <div>Age : {age} </div>
-      <div>From : {from}</div>
-    </div>
-  );
+  //   const { name, age, from } = props;
+  const { namelists } = props;
+  const nameList = namelists.namelists.map((namelist) => {
+    return (
+      <div className="vinoth">
+        <div>Name : {namelist.name}</div>
+        <div>Age : {namelist.age} </div>
+        <div>From : {namelist.from}</div>
+      </div>
+    );
+  });
+  return <div className="vinothList">{nameList}</div>;
 }
 
 export default Vinoth;
