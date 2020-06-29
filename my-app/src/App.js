13f95b889a1723361e146cec67f07dd1361dea92
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-29 18:09:01 +0530

    custom components and added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Vinoth from "./Vinoth";
 
 function App() {
   return (
     <div className="App">
       <h1>My React App!!</h1>
       <p>Welcome : )</p>
+      <Vinoth />
     </div>
   );
 }
---
+++ my-app/src/Vinoth.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Vinoth() {
+  return (
+    <div className="vinoth">
+      <div>Name : Vinoth</div>
+      <div>Age : 30 </div>
+      <div>From : Kumbakonam</div>
+    </div>
+  );
+}
+
+export default Vinoth;
