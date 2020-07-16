f029bd0d5b88e5a0131c8d4ae77c5ded8f1ff931
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-16 10:53:35 +0530

    getting values using useState

---
+++ my-app/src/AddList.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function AddList() {
+  let initialState = { name: null, age: null, form: null };
+  const [state, setState] = useState(initialState);
+  const handleChange = (e) => {
+    setState({ [e.target.id]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" onChange={handleChange} />
+        <label htmlFor="age">Age:</label>
+        <input type="text" id="age" onChange={handleChange} />
+        <label htmlFor="form">Form:</label>
+        <input type="text" id="form" onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddList;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Vinoth from "./Vinoth";
+import AddList from "./AddList";
 
 function App() {
   const [names, setNames] = useState({
@@ -16,6 +17,7 @@ function App() {
       {/* <Vinoth name="TempleCityVino" age="30" from="Kumbakonam" />
       <Vinoth name="Kanna" age="30" from="Kumbakonam" /> */}
       <Vinoth namelists={names} />
+      <AddList />
     </div>
   );
 }
