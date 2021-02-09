4d9d4d9009c652d8b47319926228752f1245fe34
vinothsmart <s.vinothkanna90@gmail.com>
2021-02-09 14:58:34 +0530

    getting values using useRef

--- todolist/src/Component/AddItem.js
+++ todolist/src/Component/AddItem.js
@@ -1,7 +1,15 @@
-function AddItem(props) {
+import React, { useRef } from "react";
+
+function AddItem() {
+  const inputNewItem = useRef(null);
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    console.log(inputNewItem.current.value);
+  };
+
   return (
-    <form id="add-toto">
-      <input type="text" required />
+    <form id="add-toto" onSubmit={handleSumbit}>
+      <input type="text" required ref={inputNewItem} />
       <input type="submit" value="Hit me" />
     </form>
   );
