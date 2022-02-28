4294a83df7c3d483b99537816ad6f2118b95b2ae
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-28 17:03:24 +0530

    rm destruction in map

--- my-app/src/Lists.js
+++ my-app/src/Lists.js
@@ -3,15 +3,15 @@ import React from "react";
 const Lists = ({ lists }) => {
   return (
     <div className="name-list">
-      {lists.map(({ id, name, age, from }) => {
-        return age > 20 ? (
-          <div className="list" key={id}>
-            <div>Name: {name}</div>
-            <div>Age: {age}</div>
-            <div>From: {from}</div>
+      {lists.map((list) =>
+        list.age > 20 ? (
+          <div className="list" key={list.id}>
+            <div>Name: {list.name}</div>
+            <div>Age: {list.age}</div>
+            <div>From: {list.from}</div>
           </div>
-        ) : null;
-      })}
+        ) : null
+      )}
     </div>
   );
 };
