4a8c39627443592b3892b38653789e49217e6df0
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-28 15:33:58 +0530

    destructing list

--- my-app/src/Lists.js
+++ my-app/src/Lists.js
@@ -4,11 +4,12 @@ export default function Lists({ lists }) {
   return (
     <div className="name-list">
       {lists.map((list) => {
+        const { id, name, age, from } = list;
         return list.age > 20 ? (
-          <div className="list" key={list.id}>
-            <div>Name: {list.name}</div>
-            <div>Age: {list.age}</div>
-            <div>From: {list.from}</div>
+          <div className="list" key={id}>
+            <div>Name: {name}</div>
+            <div>Age: {age}</div>
+            <div>From: {from}</div>
           </div>
         ) : null;
       })}
