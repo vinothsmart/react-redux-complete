8b8c883eb5c6496356d7293ea19e94683f825fe2
vinothsmart <s.vinothkanna90@gmail.com>
2023-09-06 15:43:56 +0530

    addtodo

--- todoapp/src/AddTodo.js
+++ todoapp/src/AddTodo.js
@@ -1,5 +1,28 @@
-const AddTodo = () => {
-  return <div>AddTodo</div>;
+import { memo, useCallback, useState } from 'react';
+
+const AddTodo = ({ addTodo }) => {
+  const [content, setContent] = useState('');
+  const handleChange = useCallback(e => {
+    setContent(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      addTodo(content);
+      setContent('');
+    },
+    [addTodo, content],
+  );
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Add new todo:</label>
+        <input type='text' onChange={handleChange} value={content} />
+      </form>
+    </div>
+  );
 };
 
-export default AddTodo;
+export default memo(AddTodo);
--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import { lazy, Suspense, useCallback, useState } from 'react';
 const Todos = lazy(() => import('./Todos'));
+const AddTodo = lazy(() => import('./AddTodo'));
 
 function App() {
   const [todos, setTodos] = useState([
@@ -20,11 +21,22 @@ function App() {
     [],
   );
 
+  const addTodo = useCallback(content => {
+    setTodos(preValues => [
+      ...preValues,
+      {
+        id: Math.random(),
+        content,
+      },
+    ]);
+  }, []);
+
   return (
     <div className='todo-app container'>
       <h1 className='center blue-text'>Todo's</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <Todos todos={todos} deleteTodo={handleDelete} />
+        <AddTodo addTodo={addTodo} />
       </Suspense>
     </div>
   );
