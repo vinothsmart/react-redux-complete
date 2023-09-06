cb765fce1d3ab3c9f0e2eb73a6673c5f02689924
vinothsmart <s.vinothkanna90@gmail.com>
2023-09-06 19:01:16 +0530

    about

--- todoapp/src/components/Contact.js
+++ todoapp/src/components/Contact.js
@@ -1,6 +1,11 @@
 import { memo } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Contact = () => {
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate('/about');
+  }, 2000);
   return (
     <div className='container'>
       <h4 className='center'>Contact</h4>
